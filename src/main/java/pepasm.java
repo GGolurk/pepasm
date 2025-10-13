45a97d32a1c76a52960c94d6d286a700e88391e0
NMerk <breakfast1098@gmail.com>
2025-10-13 14:05:50 -0400

    Altered maps to account for instructions starting with '0'. Added branchLocations array. Next, need to start work on file input loop.

--- src/main/java/pepasm.java
+++ src/main/java/pepasm.java
@@ -6,10 +6,16 @@ public class pepasm {
     public static void main(String[] args) {
         Map<String, Integer> instructions = Map.ofEntries(
                 entry("STBA", 15), entry("LDBA", 13), entry("STWA", 14),
-                entry("LDWA", 12), entry("ANDA", 8), entry("ASLA", 110),
-                entry("ASRA", 112), entry("STOP", 0), entry("CPBA", 11), entry("BRNE", 25)
+                entry("LDWA", 12), entry("ANDA", 8), entry("CPBA", 11), entry("BRNE", 25)
+        );
+        // Called zeroInstructions because they start with a 0.
+        // They have a separate map because they do not take an input.
+        // For example, LDBA 0x000A, i, loads 0x000A, while ASRA stands alone.
+        Map<String, String> zeroInstructions = Map.ofEntries(
+                entry("ASLA", "0A"), entry("ASRA", "0C"), entry("STOP", "00")
         );
 
         String fileName = args[0];
-    } //waaaaaaaaaaaaaa
+        Integer[] branchLocations = new Integer[0];
+    }
 }
