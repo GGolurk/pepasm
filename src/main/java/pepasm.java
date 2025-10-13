09793e766e78566164b74fc15f6343d9086179fa
NMerk <breakfast1098@gmail.com>
2025-10-12 20:10:44 -0400

    Added instructions map. Numbers with three digits should have the most significant digit truncated during execution, not ideal but what I could think of for now. How do you represent 0A as an integer????

--- src/main/java/pepasm.java
+++ src/main/java/pepasm.java
@@ -1,5 +1,15 @@
+import java.util.*;
+
+import static java.util.Map.entry;
+
 public class pepasm {
     public static void main(String[] args) {
+        Map<String, Integer> instructions = Map.ofEntries(
+                entry("STBA", 15), entry("LDBA", 13), entry("STWA", 14),
+                entry("LDWA", 12), entry("ANDA", 8), entry("ASLA", 110),
+                entry("ASRA", 112), entry("STOP", 0), entry("CPBA", 11), entry("BRNE", 25)
+        );
 
+        String fileName = args[0];
     }
 }
