421c9c8734cfe79eb47383b3f1bed7179909afe6
NMerk <breakfast1098@gmail.com>
2025-10-16 17:20:42 -0400

    Everything works now except branching. So close! I even added the 'zz' at the end. List of changes: added 'ADDA' and 'SUBA', added immediate and direct addressing modes, fixed issue where lack of leading zeroes caused an error.

--- src/main/java/pepasm.java
+++ src/main/java/pepasm.java
@@ -9,13 +9,18 @@ public class pepasm {
     public static void main(String[] args) {
         Map<String, Integer> instructions = Map.ofEntries(
                 entry("STBA", 15), entry("LDBA", 13), entry("STWA", 14),
-                entry("LDWA", 12), entry("ANDA", 8), entry("CPBA", 11), entry("BRNE", 25)
-        );
+                entry("LDWA", 12), entry("ANDA", 8), entry("CPBA", 11),
+                entry("BRNE", 25), entry("ADDA", 6), entry("SUBA", 7)
+        ); //value associated with value in pep
         // Called zeroInstructions because they start with a 0.
         // They have a separate map because they do not take an input.
         // For example, LDBA 0x000A, i, loads 0x000A, while ASRA stands alone.
         Map<String, String> zeroInstructions = Map.ofEntries(
                 entry("ASLA", "0A"), entry("ASRA", "0C"), entry("STOP", "00")
+        );
+
+        Map<String, Integer> instructionSpecifiers = Map.ofEntries(
+                entry("i", 0), entry("d", 1)
         );
 
         String fileName = args[0];
@@ -36,18 +41,53 @@ public class pepasm {
                 line = br.readLine();
 
                 // Ends loop if EOF
-                if(line.contains(".END")){
+                if(line == null || line.contains(".END")){
+                    System.out.print(" zz");
                     break;
                 }
-
+                //removes all whitespace before and after the string
+                line = line.trim();
+                if (line.isEmpty()) continue;
+                //splitting into chunks, uppercase for uniformity
                 String[] splitLine = line.split(" ");
-                for(int i = 0; i < splitLine.length; i++){
-                    System.out.println(splitLine[i]);
+                String instruction = splitLine[0].toUpperCase();
+
+                if (zeroInstructions.containsKey(instruction)) {
+                    System.out.print(zeroInstructions.get(instruction));
+                }
+
+                else if (instructions.containsKey(instruction)) {
+                    int code = instructions.get(instruction);
+                    String address;
+                    if (splitLine.length > 1) {
+                        address = splitLine[1];
+                    } else {
+                        address = "0x0000";
+                    }
+                    //getting rid of commas and prefixes
+                    address = address.replace(",", "").replace("0x", "");
+                    //convert to hex
+                    String hexCode = String.format("%01X", code);
+                    //get instruction specifier & add to instruction
+                    int specifier = instructionSpecifiers.get(splitLine[2]);
+                    hexCode += String.valueOf(specifier);
+                    System.out.print(hexCode + " ");
+                    //making sure that we have a four digit hex number
+                    while(address.length() < 4){
+                        address = "0" + address;
+                    }
+                    //separating address into 2-digit chunks & printing them
+                    for (int i = 0; i < address.length(); i += 2) {
+                        String byteChunk = address.substring(i, i + 2);
+                        System.out.print(byteChunk + " ");
+                    }
+                }
+                else {
+                    System.out.println("Invalid/Unknown Instruction: " + instruction);
                 }
             } catch (IOException e) {
                 throw new RuntimeException(e);
-            }
-
-        }
-    }
-}
+            } // End catch
+        } // End while
+    } // End main
+} // End class
